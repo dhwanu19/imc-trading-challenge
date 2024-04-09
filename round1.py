744880abc1b14d56c270a900bf6ffbe3a266c3cb
atomicabyss61 <65688519+atomicabyss61@users.noreply.github.com>
2024-04-09 15:43:53 +1000

    round 1 amethyst strategy

--- round1.py
+++ round1.py
@@ -2,7 +2,66 @@ from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import string
 
+AMETHYSTS = "AMETHYSTS"
+STARFRUIT = "STARFRUIT"
+SEASHELLS = "SEASHELLS"
+
+ASSETS = [
+    STARFRUIT,
+    SEASHELLS,
+]
+
+# Mean price specificed by IMC
+DEFAULT_PRICES = {
+    AMETHYSTS : 10_000,
+    STARFRUIT : 5_000,
+}
+
 class Trader:
+    def __init__(self) -> None:
+        print("Initialising...")
+
+        self.position_limit = (
+            AMETHYSTS : 20, 
+            STARFRUIT : 20, 
+        )
+
+        # self.round = 0
+        self.shells = 0
+
+        # List of price history of assets
+        self.price_history = dict()
+        for asset in ASSETS:
+            asset.price_history[asset] = []
+            # Set others such as expected regresion price
+        
+    def starfruit_strategy():
+        pass
+    
+    # market make/take around 10k
+    def amethyst_strategy(self, state: TradingState):
+        
+        # get position
+        amethyst_pos = self.get_pos(AMETHYSTS, state)
+
+        # find max bid/ask amount
+        bid_volume = self.position_limit[AMETHYSTS] - amethyst_pos
+        ask_volume = -1 * (self.position_limit[AMETHYSTS] + amethyst_pos)
+             
+        price_dev = 1
+        
+        # append +-1 buy/sell order
+        orders = []
+        orders.append(Order(AMETHYSTS, DEFAULT_PRICES[AMETHYSTS] - price_dev, bid_volume))
+        orders.append(Order(AMETHYSTS, DEFAULT_PRICES[AMETHYSTS] + price_dev, ask_volume))
+        
+        return orders
+    
+    def get_pos(self, product, state: TradingState):
+        # return state.position.get(product)
+        # Investigate later
+        return state.position.get(product, 0) 
+    
     
     def run(self, state: TradingState):
         # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
@@ -35,3 +94,4 @@ class Trader:
         
         conversions = 1
         return result, conversions, traderData
+
